4502d84af4b2497c3946a945ffe020188e3fd1c3
migo123321 <121448215+migo123321@users.noreply.github.com>
2023-02-21 16:38:55 +0300

    Add files via upload
    yeni hendek i seveceksiniz

--- hendek.py
+++ hendek.py
@@ -3,8 +3,9 @@
 #pip install getpass
 #bunu ben yaptım veben bir türküm mamungame
 #pip install shutil
-import shutil
 
+import shutil
+import random
 import time
 import socket
 from art import tprint
@@ -13,14 +14,15 @@ getpass("")
 tprint("hendek")
 
 print("""
-[0]error
-[1] port checker
+[0]not exit!!!
+[1]port checker
 [2]port scanner 
-[3]Which tool should come???
-[4]exit""")
+[3]password checker
+[4]password scanner
+[5]exit""")
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 option=int(input("option>>> "))
-if (option==4):
+if (option==5):
     print("exit")
     exit
 elif(option==1):
@@ -48,18 +50,31 @@ elif(option==2):
             print("[-] port close>>> ",str(porT))
             time.sleep(14)
 elif (option==3):
-    print("cupp")
-    print(" Which tool should come???              ")
-    print("  \            ")
-    print("   \         ")
-    print("    ,__,     ")        
-    print("    (oo)____ ")     
-    print("    (__)    )\\")
-    print("       ||--|| *")
+    a_a12=int(input("lenght"))
+    owet="qwertyuıopğüasdfghjklşiçömnbvcxz"
     
-    print("shutil...")
-    time.sleep(14)
-      
+    upper_lower="qwertyuıopğüasdfghjklşiçömnbvcxz"
+    NUMPERS="0123456789"
+    strings=owet+upper_lower+NUMPERS
+    length=a_a12
+    password="".join(random.sample(strings,length))
+    print("[+]password: ",password)
+    time.sleep(1)
+elif (option==4):
+    mİN=int(input("min"))
+    mAx=int(input("max"))
+    a_a12=int(input("lenght"))
+    length=a_a12
+    for i in range(mİN,mAx+1):
+       
+        owet="qwertyuıopğüasdfghjklşiçömnbvcxz"
+        upper_lower="qwertyuıopğüasdfghjklşiçömnbvcxz"
+        NUMPERS="0123456789"
+        strings=owet+upper_lower+NUMPERS
+        
+        password="".join(random.sample(strings,length))
+        print("[+]password: ",password)
+        time.sleep(1)
 else:
     print("[-] Error")
     time.sleep(5)
